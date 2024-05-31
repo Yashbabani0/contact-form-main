ae6cd468e667233de83b2b1b3a7f291468bf7850
Yash Babani <yashbabani09@gmail.com>
2024-05-31 17:13:11 +0530

    project complete

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const optionContainers = document.querySelectorAll(".options");
+const optionContainers = document.querySelectorAll(".options");
+const form = document.querySelector("form");
+const main = document.querySelector("main");
+const successfulAlert = document.querySelector(".successful_alert");
 
-  optionContainers.forEach((container) => {
-    container.addEventListener("click", () => {
-      const radioId = container.getAttribute("data-for");
-      const radioButton = document.getElementById(radioId);
-      if (radioButton) {
-        radioButton.checked = true;
-      }
-    });
+optionContainers.forEach((container) => {
+  container.addEventListener("click", () => {
+    const radioId = container.getAttribute("data-for");
+    const radioButton = document.getElementById(radioId);
+    if (radioButton) {
+      radioButton.checked = true;
+    }
   });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.style.pointerEvents = "none";
+  main.style.opacity = 0.2;
+  successfulAlert.style.display = "block";
 });
